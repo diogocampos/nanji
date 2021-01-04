e08367872acfb5bc1972861ee789854edcb45cfa
Diogo Campos <diogocampos@users.noreply.github.com>
2021-01-04 20:06:20 -0300

    Add 'Regenerate' button

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,12 +4,17 @@ import { generatePhrase } from '../../lib/nanji'
 import './App.css'
 
 function App() {
-  const [phrase] = useState(generatePhrase)
+  const [phrase, setPhrase] = useState(generatePhrase)
+
+  function handleRegenerate() {
+    setPhrase(generatePhrase())
+  }
 
   return (
     <div className='App'>
       <h1 className='title'>何時ですか？</h1>
       <p className='time'>{phrase}</p>
+      <button onClick={handleRegenerate}>Regenerate</button>
     </div>
   )
 }
