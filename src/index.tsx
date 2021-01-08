a861c7181d9c57987d1652e1b6da062958abe919
Diogo Campos <diogocampos@users.noreply.github.com>
2021-01-07 21:55:29 -0300

    Fix lint warning

--- src/index.tsx
+++ src/index.tsx
@@ -16,5 +16,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line no-console
   reportWebVitals(console.log)
 }
