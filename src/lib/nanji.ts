ca80058f7e7b869b69f5e9326284d0472abd561d
Diogo Campos <diogocampos@users.noreply.github.com>
2021-01-07 22:33:42 -0300

    Add pronunciation for hours and minutes

--- src/lib/nanji.ts
+++ src/lib/nanji.ts
@@ -10,8 +10,55 @@ const CHOUDO = R(r('ちょう', 'chō'), r('ど', 'do'))
 const GORO = R(r('ご', 'go'), r('ろ', 'ro'))
 const DESU = R(r('で', 'de'), r('す', 'su'), r('。', ''))
 
-const JI = '時'
-const FUN = '分'
+const JI = r('時', '-ji')
+const FUN = r('分', '-fun')
+const PUN = r('分', '-pun')
+
+const HOURS = [
+  R(r('0', 'zero'), JI),
+  R(r('1', 'ichi'), JI),
+  R(r('2', 'ni'), JI),
+  R(r('3', 'san'), JI),
+  R(r('4', 'yo'), JI),
+  R(r('5', 'go'), JI),
+  R(r('6', 'roku'), JI),
+  R(r('7', 'shichi'), JI),
+  R(r('8', 'hachi'), JI),
+  R(r('9', 'ku'), JI),
+  R(r('0', 'jū'), JI),
+]
+
+const MINUTES = [
+  R(),
+  R(r('1', 'ip'), PUN),
+  R(r('2', 'ni'), FUN),
+  R(r('3', 'san'), PUN),
+  R(r('4', 'yon'), PUN),
+  R(r('5', 'go'), FUN),
+  R(r('6', 'rop'), PUN),
+  R(r('7', 'nana'), FUN),
+  R(r('8', 'hap'), PUN),
+  R(r('9', 'kyū'), FUN),
+  R(r('0', 'jūp'), PUN),
+]
+
+const TENS = [
+  R(),
+  r('1', 'jū'),
+  r('2', 'ni-jū'),
+  r('3', 'san-jū'),
+  r('4', 'yon-jū'),
+  r('5', 'go-jū'),
+]
+
+const TENS_PREFIX = [
+  R(),
+  r('1', ''),
+  r('2', 'ni-'),
+  r('3', 'san-'),
+  r('4', 'yon-'),
+  r('5', 'go-'),
+]
 
 const MAX_REGEN_ATTEMPTS: number = 10
 
@@ -56,18 +103,40 @@ function generatePhrase(hour: number, minute: number): Ruby {
   const hh
     = hour === 0 && minute === 0 ? REIJI
     : hour === 12 && minute === 0 ? SHOUGO
-    : r(`${hour}${JI}`, '')
+    : getHourSegment(hour)
 
   // prettier-ignore
   const mm
     = minute === 0 ? R()
     : minute === 30 && (goro.length || coinFlip()) ? HAN
-    : r(`${minute}${FUN}`, '')
+    : getMinuteSegment(minute)
 
   const hhmm = R(hh, mm)
   const time = ampm.length || coinFlip() ? R(hhmm, choudo) : R(choudo, hhmm)
 
   return R(imawa, ampm, time, goro, DESU)
+}
+
+function getHourSegment(hour: number): Ruby {
+  return getTimeSegment(hour, HOURS)
+}
+
+function getMinuteSegment(minute: number): Ruby {
+  return getTimeSegment(minute, MINUTES)
+}
+
+function getTimeSegment(value: number, units: Ruby[]): Ruby {
+  const tens = Math.floor(value / 10)
+  const ones = value % 10
+
+  const tensRubies = ones > 0 ? R(TENS[tens]) : R()
+  // prettier-ignore
+  const onesRubies
+    = ones > 0 ? units[ones]
+    : /* ones === 0 && */ tens > 0 ? R(TENS_PREFIX[tens], units[10])
+    : /* ones === 0 && tens === 0 */ units[0]
+
+  return R(tensRubies, onesRubies)
 }
 
 function round(hour: number, minute: number) {
